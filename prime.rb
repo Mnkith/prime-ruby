76e003fffc658cad9f08f8a8ccee93204632322a
Mnkith <munkithabid@gmail.com>
2020-10-24 19:34:52 -0500

    commit tree 6b917f0370b38260d09364ede3aee7f0fd57a308

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'benchmark'
+def prime?(int)
+  return true if int == 2 
+  return false if int <= 1 || int.even?
+  i = 3
+  until i >= int**0.5 
+    return false if int % i == 0
+    i += 1
+  end
+  true
+end
+puts Benchmark.measure { prime?(9999999999999999999999999999999999) }
